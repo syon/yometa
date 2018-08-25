e80d459594b688ac5d9f140780f42441770d6971
syon <syon.xiii@gmail.com>
2018-08-25 20:28:39 +0900

    MP3 read prototype

--- App.vue
+++ App.vue
@@ -33,9 +33,11 @@
 </template>
 
 <script>
-import NodeID3 from "node-id3";
 import VueFullScreenFileDrop from "vue-full-screen-file-drop";
 import "vue-full-screen-file-drop/dist/vue-full-screen-file-drop.css";
+import jsmediatags from 'jsmediatags';
+import getMP3Duration from 'get-mp3-duration';
+import mp3Parser from 'mp3-parser';
 
 /* eslint-disable */
 export default {
@@ -46,7 +48,7 @@ export default {
   data: function() {
     return {
       files: [],
-      output: "",
+      output: ""
     };
   },
   methods: {
@@ -61,13 +63,33 @@ export default {
     readFile() {
       var file = this.files[0];
       var reader = new FileReader();
-      reader.addEventListener('load', () => { this.onFileLoad(reader) }, false);
+      reader.addEventListener(
+        "load",
+        () => {
+          this.onFileLoad(reader);
+        },
+        false
+      );
       reader.readAsArrayBuffer(file);
     },
     onFileLoad(reader) {
-      var buf = reader.result;
-      const tags = NodeID3.read(new Buffer(buf));
-      this.output = JSON.stringify(tags, null, 2);
+      var data = reader.result;
+      var buf = new Buffer(data);
+      jsmediatags.read(buf, {
+        onSuccess: function(tag) {
+          console.log(tag);
+          this.output = JSON.stringify(tag, null, 2);
+        },
+        onError: function(error) {
+          console.log(':(', error.type, error.info);
+        }
+      });
+      const duration = getMP3Duration(buf)
+      console.log("------------------------------")
+      console.log(duration);
+      console.log("------------------------------")
+      var mp3Tags = mp3Parser.readTags(new DataView(reader.result));
+      console.log(mp3Tags);
     },
     handleDownload() {
       var file = this.files[0];
